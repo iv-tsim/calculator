26b96fba909f0d68c9decc131263146a8053e66f
iv-tsim <vanich2001@yandex.ru>
2020-12-08 08:37:52 +0300

    final

--- js/main.js
+++ js/main.js
@@ -53,9 +53,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     //склонение дней
-    declOfNum = (n, titles) => {
+    const declOfNum = (n, titles) => {
+        
         return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
                                 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+
     }   
 
     hideElem(calc);
